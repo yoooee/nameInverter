dad3da533492aed31d431c8b26bd560b34f82b2c
Josef Staroba <jstaroba@centene.com>
2019-01-03 13:16:56 -0600

    Add second basic test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,9 +8,12 @@ describe('nameInverter', () => {
   beforeEach(() => {
   });
 
-  it('given Null, returns Emptry String', () => {
+  it('given Null, returns Empty string', () => {
     expect(invertName(null)).toEqual('');
   });
 
+  it('given Empty string, returns Empty string', () => {
+    expect(invertName('')).toEqual('');
+  });
 
 });
